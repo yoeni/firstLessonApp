6b529c6d776a08abac5300a054fd0e7ce757725b
yoeni <m.sezeryildirim@gmail.com>
2021-05-23 16:39:30 +0300

    may 2021

--- app/src/main/java/online/sezeryildirim/firstlessonapp/firstFragment.java
+++ app/src/main/java/online/sezeryildirim/firstlessonapp/firstFragment.java
@@ -1,12 +1,35 @@
 package online.sezeryildirim.firstlessonapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.InputStream;
+
+import static android.content.ContentValues.TAG;
 
 public class firstFragment extends Fragment {
 
@@ -14,10 +37,63 @@ public class firstFragment extends Fragment {
     public firstFragment() {
     }
 
+    private DatabaseReference mDatabase;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView= inflater.inflate(R.layout.fragment_first, container, false);
+        mDatabase=FirebaseDatabase.getInstance().getReference().child("haberler").child("2");
+
+        TextView title= rootView.findViewById(R.id.titleText);
+        TextView p1= rootView.findViewById(R.id.p1);
+        TextView p2= rootView.findViewById(R.id.p2);
+        ImageView titleImage=rootView.findViewById(R.id.titleImage);
+        ImageView image2=rootView.findViewById(R.id.image2);
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                title.setText(dataSnapshot.child("title").getValue().toString());
+                p1.setText(dataSnapshot.child("p1").getValue().toString());
+                p2.setText(dataSnapshot.child("p2").getValue().toString());
+                new DownloadImageTask(titleImage).execute(dataSnapshot.child("image1").getValue().toString());
+                new DownloadImageTask(image2).execute(dataSnapshot.child("image2").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mDatabase.addValueEventListener(postListener);
+
+        /*Glide.with(this).load(getValue("image1")).into(titleImage);
+        Glide.with(this).load(getValue("image2")).into(image2);
+        new DownloadImageTask(titleImage).execute(getValue("image1"));
+        new DownloadImageTask(image2).execute(getValue("image2"));*/
+        
         return rootView;
     }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+        public DownloadImageTask(ImageView bmImage) {this.bmImage = bmImage;}
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("RESİM ALMA HATASI", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
 }
